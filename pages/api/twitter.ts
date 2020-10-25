829c75fa0f4eeb3a4fddf6055fde7e27416aff38
PraZ <sachabouillez@gmail.com>
2020-10-25 15:42:25 +0100

    :construction: change handler

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -4,7 +4,8 @@ const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   if (_req.method != "POST") res.status(405).send("Only POST allowed.");
   console.log("received request");
   console.log(_req.body);
-  res.status(200).send("ok");
+  res.statusCode = 200;
+  res.json({ message: "ok" });
 };
 
 export default handler;
