3aa0860c9b4fc111e57fbf326f6c12363381200d
PraZ <sachabouillez@gmail.com>
2020-10-25 15:35:25 +0100

    :loud_sound: add more logs

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
-  if(_req.method != 'POST') res.status(405).send('Only POST allowed.')
+  if (_req.method != "POST") res.status(405).send("Only POST allowed.");
+  console.log("received request");
   console.log(_req.body);
   res.status(200).send("ok");
 };
