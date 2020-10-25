d5bbfbf6a5d314d8c62a17c3c11f4288edc7f205
PraZ <sachabouillez@gmail.com>
2020-10-25 15:22:53 +0100

    :construction: limit to post

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  if(_req.method != 'POST') res.status(405).send('Only POST allowed.')
   console.log(_req.body);
   res.status(200).send("ok");
 };
